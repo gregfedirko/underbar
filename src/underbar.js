50d5641fb5405d3348a7ce0b1e3b029e90d16cee
Greg Fedirko <gjfedirko@gmail.com>
2014-08-01 17:43:38 -0700

    completed _.filter()

--- src/underbar.js
+++ src/underbar.js
@@ -87,6 +87,13 @@ var _ = {};
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var newArr = [];
+    _.each(collection, function(val){
+      if (test(val)){
+        newArr.push(val);
+      }
+    });
+    return newArr;
   };
 
   // Return all elements of an array that don't pass a truth test.
